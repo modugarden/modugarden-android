1a65f186cc968d61ff2c1f24345ed97e7db49724
이은섭 <dldmstjq99@gmail.com>
2023-01-16 17:14:46 +0900

    add : ProfileData (프로필 테스트용)

---
+++ app/src/main/java/com/example/modugarden/main/profile/ProfileData.kt
@@ -0,0 +1,28 @@
+package com.example.modugarden.main.profile
+
+data class User(
+    val image: Int,
+    val name: String,
+    val category: List<String>,
+    val follower: Int,
+    val following: Int,
+    val state: Boolean,
+    val post: List<PostCard>,
+    val curation: List<CurationCard>
+)
+
+data class PostCard(
+    val image: Int = 0,
+    val title: String = "Default Title",
+    val category: String = "Default Category",
+    val time: String = "No time",
+    val user: String = "Default UserName"
+)
+
+data class CurationCard(
+    val thumbnail_image: Int,
+    val title: String,
+    val category: String,
+    val time: String,
+    val user: String
+)
