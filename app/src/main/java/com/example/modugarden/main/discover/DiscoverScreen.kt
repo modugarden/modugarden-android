667f7ba82a72b26318f392735ce845d625b57c4e
kimtaeyoon20031220 <k0278kim@gmail.com>
2023-01-17 20:36:35 +0900

    modify: [AndroidManifest.xml] 퍼미션 추가, cleartexttraffic 추가.

--- app/src/main/java/com/example/modugarden/main/discover/DiscoverScreen.kt
+++ app/src/main/java/com/example/modugarden/main/discover/DiscoverScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.modugarden.main.discover.search.Categories
-
 import com.example.modugarden.main.discover.search.DiscoverSearchScreen
 
 
