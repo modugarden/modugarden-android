824cbfc0e91647880c13704cc0cbcba88f433d44
이은섭 <dldmstjq99@gmail.com>
2023-01-23 20:59:03 +0900

    add : 저장한 항목 액티비티 modify : 큐레이터일 경우와 아닌 경우, 내 프로필인지 아닌지에 따른 프로필 화면 구성 및 버튼들에 바운스클릭 부여

--- app/src/main/java/com/example/modugarden/main/profile/ProfileSaveActivity.kt
+++ app/src/main/java/com/example/modugarden/main/profile/ProfileSaveActivity.kt
@@ -3,19 +3,29 @@ package com.example.modugarden.main.profile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.example.modugarden.R
+import com.example.modugarden.ui.theme.TopBar
 
 class ProfileSaveActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             // 인텐트로 포스트, 큐레이션 리스트 받아옴
-            Surface(
+            Column (
                 modifier = Modifier.fillMaxSize()
             ) {
-
+                TopBar(
+                    title = "저장한 항목",
+                    titleIcon = R.drawable.ic_arrow_left_bold,
+                    titleIconOnClick = {
+                        finish()
+                    }
+                )
+                CuratorProfileTab()
             }
         }
     }
