3ea45e1f2cb144aa7b8e6541999bb29b00093205
OJOJIN <ojy101039@gmail.com>
2023-01-24 19:31:10 +0900

    add: [DiscoverSearchingScreen.kt, DiscoverSearchResult.kt] 검색결과에서 글자 수가 너무 많다면 ... 처리기능 추가, 유저검색 팔로우 스낵바 추가

--- app/src/main/java/com/example/modugarden/main/discover/search/DiscoverSearchUser.kt
+++ app/src/main/java/com/example/modugarden/main/discover/search/DiscoverSearchUser.kt
@@ -3,20 +3,22 @@ package com.example.modugarden.main.discover.search
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
 
 
 @Composable
-fun DiscoverSearchUser(searchStr : String){
+fun DiscoverSearchUser(searchStr : String, coroutineScope: CoroutineScope, snackbarHostState: SnackbarHostState){
 
     LazyColumn(modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(horizontal = 18.dp, vertical = 18.dp)
     ) {
         items(
-            count = 8,
-            itemContent = { DiscoverSearchUserCard(searchStr) }
+            count = 15,
+            itemContent = { DiscoverSearchUserCard(searchStr, coroutineScope, snackbarHostState) }
         )
     }
 }
