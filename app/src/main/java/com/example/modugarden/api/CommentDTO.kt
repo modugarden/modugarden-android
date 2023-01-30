ffa8e5445fb8af2d39b9c34fd70b1952563ee154
joowojr <joowojr@gmail.com>
2023-01-30 10:46:02 +0900

    add: [CommentDTO.kt], [CommentAPI.kt], [CurationAPI.kt]

---
+++ app/src/main/java/com/example/modugarden/api/CommentDTO.kt
@@ -0,0 +1,16 @@
+package com.example.modugarden.api
+
+
+data class CommentDTO(
+    val code: Int,
+    val isSuccess: Boolean,
+    val messege :String,
+    val result : Int //commentId
+    )
+
+data class GetCommentsResult(
+    val content : Int, //commentId
+    val first : Boolean,
+    val hasNext : Boolean,
+    val last : Boolean
+    )
