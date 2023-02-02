bc643be8ee3ed882b8f80c8648a90415bfb46445
joowojr <joowojr@gmail.com>
2023-02-02 14:57:48 +0900

    modify : CommentAPI, CommentDTO 작성 완료

--- app/src/main/java/com/example/modugarden/api/api/CommentAPI.kt
+++ app/src/main/java/com/example/modugarden/api/api/CommentAPI.kt
@@ -1,23 +1,26 @@
 package com.example.modugarden.api.api
 
 import com.example.modugarden.api.dto.CommentDTO
+import com.example.modugarden.api.dto.GetCommentResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface CommentAPI {
     @POST("/boards/{board_id}/comments")
     fun sendComment(
-        @Body boardId : Int ,
+        @Path ("board_id") board_id : Int,
         @Body content : String,
         @Body parentId : Int
     ): Call<CommentDTO>
 
-/*    @GET("/boards/{board_id}/comments")
+   @GET("/boards/{board_id}/comments")
     fun getComments(
         @Body boardId : Int
-    ) : Call<GetCommentsResult>*/
+    ) : Call<GetCommentResponse>
 
     @PATCH("/boards/{board_id}/comments/{comment_id}")
     fun editComment(
