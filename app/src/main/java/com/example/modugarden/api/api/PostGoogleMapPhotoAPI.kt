4e70768a1e63f0b992df3e1b08444cdef2ee62df
joowojr <joowojr@gmail.com>
2023-02-05 05:37:37 +0900

    add: PostGoogleMapPhotoAPI/ RetrofitBuilder 추가

---
+++ app/src/main/java/com/example/modugarden/api/api/PostGoogleMapPhotoAPI.kt
@@ -0,0 +1,22 @@
+package com.example.modugarden.api.api
+
+import com.example.modugarden.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+/*
+interface PostGoogleMapPhotoAPI {
+    @GET("/place/photo")
+
+}
+
+interface PostGoogleFAPI{
+    @GET("/place/findplacefromtext/json")
+    fun getPhotoRefernce(
+        @Query("geometry") address: String,
+        @Query("key") key: String = BuildConfig.google_maps_key,
+    ):CAll<>
+}
+
+data class PhotoReference()*/
