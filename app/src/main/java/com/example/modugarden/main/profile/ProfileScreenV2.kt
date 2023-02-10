d05365f896aece7b1c6dae2f3238f3d2eace6ed7
이은섭 <dldmstjq99@gmail.com>
2023-02-11 01:25:18 +0900

    Add : ProfileScreenV4.kt 추가 : 탭 고정헤더로 2중스크롤 가능하게 함

--- app/src/main/java/com/example/modugarden/main/profile/ProfileScreenV2.kt
+++ app/src/main/java/com/example/modugarden/main/profile/ProfileScreenV2.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
