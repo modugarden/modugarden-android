f50e0337b91a9620bf268a26a2941ba150ea86ff
OJOJIN <ojy101039@gmail.com>
2023-02-12 18:01:22 +0900

    add : splash 스크린 추가

--- app/src/main/java/com/example/modugarden/login/LoginActivity.kt
+++ app/src/main/java/com/example/modugarden/login/LoginActivity.kt
@@ -1,16 +1,30 @@
 package com.example.modugarden.login
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.rememberNavController
+import com.example.modugarden.R
 import com.example.modugarden.route.NavigationGraphLogin
+import com.example.modugarden.ui.theme.moduPoint
 
 class LoginActivity: ComponentActivity() {
 
